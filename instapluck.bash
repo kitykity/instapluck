e1aadc00dbd9c958bef7c50a744d015de4fcbeae
Susan <susan.a.pitman@gmail.com>
2014-11-18 08:09:34 -0600

    Update instapluck.bash

--- instapluck.bash
+++ instapluck.bash
@@ -23,7 +23,10 @@ sed 's/\<a href\=\"/\
 \
 /g' export.html | grep "http://instagram.com/" | cut -c1-80 | grep "aria-haspopup" | cut -d"\"" -f1 > ${thisDir}/doig.links
 
+echo "List of post links created."
 while read thisLink ; do
-  curl ${thisLink}
-  sleep 5
+#  wget ${thisLink} | tail -100 | head
+  echo ${thisLink}
+  sleep 1
 done < ${thisDir}/doig.links
+

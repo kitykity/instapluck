0c29897f424d3f99e56d9b2b31fe571d9b1600da
Susan <susan.a.pitman@gmail.com>
2014-11-18 08:30:11 -0600

    Update instapluck.bash

--- instapluck.bash
+++ instapluck.bash
@@ -19,14 +19,20 @@
 #
 #!/bin/bash
 thisDir=`pwd`
-sed 's/\<a href\=\"/\
-\
-/g' export.html | grep "http://instagram.com/" | cut -c1-80 | grep "aria-haspopup" | cut -d"\"" -f1 > ${thisDir}/doig.links
-
-echo "List of post links created."
+#echo "Creating list of links..."
+#sed 's/\<a href\=\"/\
+#\
+#/g' export.html | grep "http://instagram.com/" | cut -c1-80 | grep "aria-haspopup" | cut -d"\"" -f1 > ${thisDir}/doig.links
+#
+#echo "List of post links created."
 while read thisLink ; do
-#  wget ${thisLink} | tail -100 | head
+#  wget ${thisLink} -O ${thisDir}/index.html | tail -100 | head
+  cat ${thisDir}/index.html |grep app_platform | sed 's/\"caption\"\:\"/\
+/g' | grep shared_by_author | sed 's/\"\,\".*$//g' > ${thisDir}/caption
+  imageLink=`grep "og:image" index.html | cut -d"=" -f3 | sed 's/\"//g' | sed 's/ \/>//g'`
   echo ${thisLink}
+  echo ${imageLink}
+  cat ${thisDir}/caption
+  echo
   sleep 1
 done < ${thisDir}/doig.links
-

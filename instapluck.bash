f8584540325e4827faaf969f3a7cd0e291d45876
Susan <susan.a.pitman@gmail.com>
2014-11-19 15:04:45 -0600

    Converted into primitive xml format.

--- instapluck.bash
+++ instapluck.bash
@@ -1,13 +1,14 @@
 #!/bin/bash
 thisDir=`pwd`
 
+printf "\n<xml>\n\n"
 while read pageLink ; do
-  wget ${pageLink} -O ${thisDir}/index.html | tail -100 | head
+  wget -q ${pageLink} -O ${thisDir}/index.html
   imageLink=`grep "og:image" ${thisDir}/index.html | cut -d"=" -f3 | sed 's/\"//g' | sed 's/ \/>//g'`
   imageCaption=`grep "\"caption\"" ${thisDir}/index.html | sed 's/.*caption\"\:\"//g' | cut -d"\"" -f1`
-  echo ${pageLink}
-  echo ${imageLink}
-  echo ${imageCaption}
-  echo
-  sleep 10
+  epochDate=`grep "\"date\"" ${thisDir}/index.html | sed 's/.*\"date\"\://' | cut -d"." -f1`
+  imageDate=`date -d @${epochDate} '+%m/%d/%Y'`
+  printf "  <item>\n    <pagelink>${pageLink}</pagelink>\n    <imagedate>${imageDate}</imagedate>\n    <imagelink>${imageLink}<imagelink>\n    <imagecaption>${imageCaption}</imagecaption>\n  </item>\n\n"
+  sleep 6
 done < ${thisDir}/links.instapluck
+printf "</xml>\n\n"

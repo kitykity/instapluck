c35799d0093f529afe67fd909488eb7bbfff6626
Susan <susan.a.pitman@gmail.com>
2014-11-19 12:40:10 -0600

    Update instapluck.bash

--- instapluck.bash
+++ instapluck.bash
@@ -1,38 +1,13 @@
-#
-#
-#
-#
-           ' NOTE!!! This script is a work in progress and IS NOT COMPLETE!!! '
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
 #!/bin/bash
 thisDir=`pwd`
-#echo "Creating list of links..."
-#sed 's/\<a href\=\"/\
-#\
-#/g' export.html | grep "http://instagram.com/" | cut -c1-80 | grep "aria-haspopup" | cut -d"\"" -f1 > ${thisDir}/doig.links
-#
-#echo "List of post links created."
-while read thisLink ; do
-#  wget ${thisLink} -O ${thisDir}/index.html | tail -100 | head
-  cat ${thisDir}/index.html |grep app_platform | sed 's/\"caption\"\:\"/\
-/g' | grep shared_by_author | sed 's/\"\,\".*$//g' > ${thisDir}/caption
-  imageLink=`grep "og:image" index.html | cut -d"=" -f3 | sed 's/\"//g' | sed 's/ \/>//g'`
-  echo ${thisLink}
+
+while read pageLink ; do
+  wget ${pageLink} -O ${thisDir}/index.html | tail -100 | head
+  imageLink=`grep "og:image" ${thisDir}/index.html | cut -d"=" -f3 | sed 's/\"//g' | sed 's/ \/>//g'`
+  imageCaption=`grep "\"caption\"" ${thisDir}/index.html | sed 's/.*caption\"\:\"//g' | cut -d"\"" -f1`
+  echo ${pageLink}
   echo ${imageLink}
-  cat ${thisDir}/caption
+  echo ${imageCaption}
   echo
-  sleep 1
-done < ${thisDir}/doig.links
+  sleep 10
+done < ${thisDir}/links.instapluck

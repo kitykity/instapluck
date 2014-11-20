744a345f75562cf8347c81e3bff251d8a47c115d
Susan <susan.a.pitman@gmail.com>
2014-11-19 18:14:39 -0600

    Updated curl and date for Mac.

--- instapluck.bash
+++ instapluck.bash
@@ -1,14 +1,17 @@
 #!/bin/bash
+# instapluck.bash by Susan Pitman
+# Create a really primitive XML file from a list of instagram links.
+# 11/19/2014: script created
 thisDir=`pwd`
 
 printf "\n<xml>\n\n"
 while read pageLink ; do
-  wget -q ${pageLink} -O ${thisDir}/index.html
+  curl -s ${pageLink} -o ${thisDir}/index.html
   imageLink=`grep "og:image" ${thisDir}/index.html | cut -d"=" -f3 | sed 's/\"//g' | sed 's/ \/>//g'`
   imageCaption=`grep "\"caption\"" ${thisDir}/index.html | sed 's/.*caption\"\:\"//g' | cut -d"\"" -f1`
   epochDate=`grep "\"date\"" ${thisDir}/index.html | sed 's/.*\"date\"\://' | cut -d"." -f1`
-  imageDate=`date -d @${epochDate} '+%m/%d/%Y'`
+  imageDate=`date -r ${epochDate} '+%m/%d/%Y'`
   printf "  <item>\n    <pagelink>${pageLink}</pagelink>\n    <imagedate>${imageDate}</imagedate>\n    <imagelink>${imageLink}<imagelink>\n    <imagecaption>${imageCaption}</imagecaption>\n  </item>\n\n"
-  sleep 6
+  sleep 5
 done < ${thisDir}/links.instapluck
 printf "</xml>\n\n"
